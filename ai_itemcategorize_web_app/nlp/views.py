a2cdfedf624498768bedbaf8d9bb74e7879e65c4
kambe.kenta <kenta.k199405@gmail.com>
2021-01-18 19:32:54 +0900

    created home view.

--- ai_itemcategorize_web_app/nlp/views.py
+++ ai_itemcategorize_web_app/nlp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    if request.method == "GET":
+        return render(
+            request,
+            "nlp/home.html"
+        )
+    else:
+        title = [request.POST["title"]]
+        return render(
+            request,
+            "nlp/home.html",
+            {"title": title}
+        )
